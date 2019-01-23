d23716ea00d622190f23ba7a1c872880f4a1de88
Andrew MacDonald <mac087@gmail.com>
2019-01-22 20:00:00 -0600

    Created calendar component

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Calendar from "./Calendar";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Calendar />
+      </div>
+    );
+  }
+}
+
+export default App;
