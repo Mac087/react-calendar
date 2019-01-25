ada9f245fe6a30839b19caf71a7f5824070489bb
Andrew MacDonald <mac087@gmail.com>
2019-01-24 18:41:52 -0800

    Added days of the week function

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -29,7 +29,22 @@ class Calendar extends React.Component {
     );
   }
 
-  renderDays() { }
+  renderDays() {
+    const dateFormat = "dddd";
+    const days = [];
+
+    let startDate = dateFns.startOfWeek(this.state.currentMonth);
+
+    for (let i = 0; i < 7; i++) {
+      days.push(
+        <div className="col col-center" key={i}>
+          {dateFns.format(dateFns.addDays(startDate, i), dateFormat)}
+        </div>
+      );
+    }
+
+    return <div className="days row">{days}</div>;
+  }
 
   renderCells() { }
 
